eaca7015bebfaf4f30e0f9c764bd74ee2d3a5f59
Adnrew <ado.ponechal@gmail.com>
2023-12-10 22:56:00 +0100

    delete comments

--- backend/api.php
+++ backend/api.php
@@ -7,16 +7,9 @@ include_once 'config.php';
 include_once 'router.php';
 include_once CONTROLLERS_PATH . 'Authenticator.php';
 
-
-$auth = new Authenticator();
-
 $router = new Router();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // get controller
     $controller = $router->getController($_SERVER['REQUEST_URI']);
     $requestData = json_decode(file_get_contents('php://input'), true);
     $controller->processRequest($requestData);
 }
-
-// $auth->login('Bro', 'aassaa');
-// $auth->logout();
