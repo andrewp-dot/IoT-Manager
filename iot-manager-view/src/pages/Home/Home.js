d9b5984b134fac4f49d8af0db30adfd98d708dd2
Adnrew <ado.ponechal@gmail.com>
2023-12-14 14:54:17 +0100

    client side retrieve user systems

--- iot-manager-view/src/pages/Home/Home.js
+++ iot-manager-view/src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { UserContextConsumer } from '../../context/UserContext';
 import BasicPage from '../BasicPage';
 
 const Home = () => {
--- iot-manager-view/src/pages/Systems/Systems.js
+++ iot-manager-view/src/pages/Systems/Systems.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import BasicPage from '../BasicPage';
 import { Navigate } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
+import config from '../../config.json';
 import Card from '../../UI/Card';
 import cls from './styles/systems.module.css';
 
@@ -11,24 +12,66 @@ import cls from './styles/systems.module.css';
  */
 const SystemsPage = () => {
 	const userCtx = useContext(UserContext);
+	const [loading, setLoading] = useState(false);
 	if (userCtx.user.role.trim() === 'guest') {
 		return <Navigate to='/pageNotFound' />;
 	}
 
+	const getUserSystems = async () => {
+		setLoading(true);
+
+		let systems = [];
+		try {
+			const response = await fetch(config.api.systems, {
+				method: 'GET',
+				mode: 'cors',
+				cache: 'no-cache',
+				credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json',
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				redirect: 'follow', // manual, *follow, error
+				referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when);
+				withCredentials: true,
+			});
+			// get response and set data
+			if (response.ok) {
+				systems = await response.json();
+				console.log(systems);
+			} else {
+				const errorMessage = await response.json();
+				console.log(errorMessage);
+			}
+		} catch (e) {
+			console.log(e);
+		}
+
+		setLoading(false);
+		return systems || [];
+	};
+
 	const testArr = ['Test', 'Latka', 'Tutu', 'Sesky', 'Danko'];
+
+	var content;
+
+	if (loading) {
+		content = 'Loading...';
+	} else {
+		content = testArr.map((item) => {
+			return (
+				<Card width='200px'>
+					<p style={{ height: '200px', color: 'var(--color-dark-grey)' }}>
+						{item}
+					</p>
+				</Card>
+			);
+		});
+	}
+
 	return (
 		<BasicPage>
-			<div className={cls['systems']}>
-				{testArr.map((item) => {
-					return (
-						<Card width='200px'>
-							<p style={{ height: '200px', color: 'var(--color-dark-grey)' }}>
-								{item}
-							</p>
-						</Card>
-					);
-				})}
-			</div>
+			<div className={cls['systems']}>{content}</div>
 		</BasicPage>
 	);
 };
