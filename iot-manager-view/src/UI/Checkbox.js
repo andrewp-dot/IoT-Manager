43b67a14f7159ab674ea9e959a87ce9a5bbe6b1d
Adnrew <ado.ponechal@gmail.com>
2023-12-15 23:01:07 +0100

    animated switch params

--- iot-manager-view/src/UI/Checkbox.js
+++ iot-manager-view/src/UI/Checkbox.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cls from './styles/checkbox.module.css';
 
-const Checkbox = () => {
+const Checkbox = ({ value, getValue }) => {
+	const [checked, setChecked] = useState(value);
 	return (
 		<label htmlFor='checkbox' className={cls['toogle']}>
 			<input
@@ -9,8 +10,10 @@ const Checkbox = () => {
 				id='checkbox'
 				type='checkbox'
 				onChange={() => {
-					console.log('Value changed!');
+					setChecked(!checked);
+					getValue();
 				}}
+				checked={checked}
 			/>
 			<span className={cls['slider']} />
 		</label>
--- iot-manager-view/src/pages/Profile/ProfilePage.js
+++ iot-manager-view/src/pages/Profile/ProfilePage.js
@@ -45,7 +45,6 @@ const ProfilePage = () => {
 					</LogoutButton>
 				</div>
 			</div>
-			<Checkbox />
 		</ProtectedPage>
 	);
 };
