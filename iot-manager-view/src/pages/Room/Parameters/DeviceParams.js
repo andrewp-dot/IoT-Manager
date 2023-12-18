3057fdb294d8683e8a92eb6c26226ab185d83706
Adnrew <ado.ponechal@gmail.com>
2023-12-18 17:14:19 +0100

    params fronted prepare

--- iot-manager-view/src/pages/Room/Parameters/DeviceParams.js
+++ iot-manager-view/src/pages/Room/Parameters/DeviceParams.js
@@ -4,17 +4,44 @@
  */
 
 import React from 'react';
+import SingleParam from './SingleParam';
 import cls from './styles/deviceParams.module.css';
 
 const DeviceParams = ({ params }) => {
+	params = [
+		{ id: 1, type: 'state' },
+		{ id: 2, type: 'setting' },
+		{ id: 3, type: 'function' },
+	];
+
+	/* parse states */
+	const states = params.filter((item) => item.type === 'function');
+	const currentStates = states.map((param) => {
+		return <SingleParam key={param.id} type={param.type} />;
+	});
+
+	/* parse functions */
+	const functions = params.filter((item) => item.type === 'state');
+	const currentFunctions = functions.map((param) => {
+		return <SingleParam key={param.id} type={param.type} />;
+	});
+
+	/* parse settings */
+	const settings = params.filter((item) => item.type === 'setting');
+	const currentSettings = settings.map((param) => {
+		return <SingleParam key={param.id} type={param.type} />;
+	});
+
 	return (
 		<div className={cls['device-params']}>
-			<h4>Parameters</h4>
-			<div>Current State</div>
+			<p className={cls['mini-header']}>Current State</p>
+			{currentStates}
 			<div>
-				Settings
+				<p className={cls['mini-header']}>Settings</p>
 				<div>Sliders</div>
-				<div>Functions on off</div>
+				{currentSettings}
+				<p className={cls['mini-header']}></p>
+				{currentFunctions}
 			</div>
 		</div>
 	);
--- iot-manager-view/src/pages/Room/Parameters/SingleParam.js
+++ iot-manager-view/src/pages/Room/Parameters/SingleParam.js
@@ -1,8 +1,66 @@
 import React from 'react';
 import cls from './styles/deviceParams.module.css';
 
-const SingleParam = ({ devid, paramid, name, value }) => {
-	return <div cls={cls['param']}>Param in here</div>;
+/**
+ * Switchable functionality parameter component
+ * @brief Turns on / off function of the device
+ *
+ * @param  devid
+ * @param  paramid
+ * @param  name
+ * @param  value
+ * @returns
+ */
+const functionParam = (paramid, name, value) => {
+	return <div></div>;
+};
+
+/**
+ * Setting parameter component
+ * @brief Set value of the parameter (for example, temperature of air condition)
+ *
+ * @param  devid
+ * @param  paramid
+ * @param  name
+ * @param  value
+ * @returns
+ */
+const settingParam = (paramid, name, value, minVal, maxVal) => {
+	return <div></div>;
+};
+
+/**
+ * State parameter component
+ *
+ * @brief Displays current value of parameter
+ * @param  paramid parameter id
+ * @param  name name of the parameter
+ * @param  value current value
+ * @param  minVal minimum value for setting
+ * @param  maxVal maximum value for setting
+ * @returns
+ */
+const stateParam = (paramid, name, value) => {
+	return <div></div>;
+};
+
+const SingleParam = ({ paramid, name, value, type, minVal, maxVal }) => {
+	const getParamComponent = (type) => {
+		switch (type) {
+			case 'state':
+				return stateParam(paramid, name, value, type);
+			case 'function':
+				return functionParam(paramid, name, value, type);
+			case 'setting':
+				return settingParam(paramid, name, value, minVal, maxVal);
+			default:
+				break;
+		}
+		return null;
+	};
+
+	const paramItem = getParamComponent(type);
+	return <div className={cls['param']}>{paramItem}</div>;
 };
 
 export default SingleParam;
